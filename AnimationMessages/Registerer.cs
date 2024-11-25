4ade7b567a0f91044af5374b1251cac781025562
Mikhail Kortikov <kortikov.m.r@gmail.com>
2024-11-26 01:26:16 +0300

    InitializeOnLoad

--- AnimationMessages/Registerer.cs
+++ AnimationMessages/Registerer.cs
@@ -1,14 +1,18 @@
 using DingoUnityExtensions.GlobalProjectConfiguration;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 namespace DingoUnityProjectMastering.AnimationMessages
 {
+#if UNITY_EDITOR
+    [InitializeOnLoad]
     public static class Registerer
     {
-        [InitializeOnLoadMethod]
-        public static void Register()
+        static Registerer()
         {
             ScriptableObjectsRootSettings.TryAddKey(nameof(AnimationMessages));
         }
+#endif
     }
 }
